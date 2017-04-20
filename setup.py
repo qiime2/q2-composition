ec1549b4447631d195cfb9109efcfe64678f5fe2
Evan Bolyen <ebolyen@gmail.com>
2017-04-20 14:10:02 -0700

    CI/CD: Support for automation (#35)
    To hook up to the new CI/CD system, the following changes were made:
    
    - Added/updated conda recipe
    - Every commit is now a version thanks to versioneer
    - Removed `install_requires` from `setup.py` (dependencies now handled by conda recipe)
    - Standardized travis configuration to use latest development builds of QIIME 2
    
    Pair programmed with: @jairideout, @ebolyen, and @thermokarst

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@
 
 from setuptools import setup, find_packages
 
+import versioneer
+
 
 setup(
     name="q2-composition",
-    version="2017.3.0.dev",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-    install_requires=['qiime2 == 2017.3.*', 'q2-types == 2017.3.*', 'bokeh',
-                      'biom-format >= 2.1.5, < 2.2.0', 'scipy', 'scikit-bio'],
     author="Jamie Morton",
     author_email="jamietmorton@gmail.com",
     description="Compositional statistics plugin for QIIME2.",
@@ -23,5 +24,6 @@ setup(
     entry_points={
         'qiime2.plugins':
         ['q2-composition=q2_composition.plugin_setup:plugin']
-    }
+    },
+    zip_safe=False,
 )
