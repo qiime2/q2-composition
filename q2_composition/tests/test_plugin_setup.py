5304ceb5bb82161858ad586c6d3f0fcbd1d2a2dd
Matthew Dillon <matthewrdillon@gmail.com>
2017-08-01 10:51:34 -0700

    SQUASH ME

---
+++ q2_composition/tests/test_plugin_setup.py
@@ -0,0 +1,17 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2017, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import unittest
+
+from q2_composition.plugin_setup import plugin as composition_plugin
+
+
+class PluginSetupTests(unittest.TestCase):
+
+    def test_plugin_setup(self):
+        self.assertEqual(composition_plugin.name, 'composition')
