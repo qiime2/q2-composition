9f8c477b0548feaaa62d41b4f499662e68796b85
Liz Gehret <elizabeth.gehret@gmail.com>
2022-06-27 12:54:53 -0700

    remainder of pipeline for ancombc R wrapper script

--- q2_composition/assets/run_ancombc.R
+++ q2_composition/assets/run_ancombc.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+
 # load libraries -----------------
 suppressWarnings(library(phyloseq))
 suppressWarnings(library(tidyverse))
@@ -23,6 +24,7 @@ conserve            <- as.logical(args[[12]])
 alpha               <- as.numeric(args[[13]])
 global              <- as.logical(args[[14]])
 output              <- args[[15]]
+
 # load data ----------------------
 if(!path.exists(inp.abundances.path)) {
   errQuit("Input file path does not exist.")
@@ -40,4 +42,31 @@ OTU <- otu_table(otu.file, taxa_are_rows=TRUE)
 MD <- sample_data(metadata.file)
 row.names(MD) <- rownames(metadata.file)
 data <- phyloseq(OTU, MD)
+
 # analysis -----------------------
+fit=ancombc(data, formula, p_adj_method)
+
+# extract multivariate regression coefficients from the structure
+beta  <- fit$res$beta    # beta
+se    <- fit$res$se      # standard error
+w     <- fit$res$W       # test statistic
+p_val <- fit$res$p_val   # p-value
+q_val <- fit$res$q_val   # q-value
+
+# adding descriptors to each column
+colnames(beta) <- modify(colnames(beta),
+        function(x){return(paste(x, 'beta', sep='_'))})
+colnames(se) <- modify(colnames(se),
+        function(x){return(paste(x, 'se', sep='_'))})
+colnames(w) <- modify(colnames(w),
+        function(x){return(paste(x, 'W', sep='_'))})
+colnames(p_val) <- modify(colnames(p_val),
+        function(x){return(paste(x, 'p-value', sep='_'))})
+colnames(q_val) <- modify(colnames(q_val),
+        function(x){return(paste(x, 'q-value', sep='_'))})
+
+# Concat everything into a distance matrix
+diffs <- as.data.frame(cbind(beta, se, w, p_val, q_val))
+
+# Write distance matrix to file
+write.csv(diffs, file=output)
