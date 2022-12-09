3d1d8da3b755efa923769ae1dcbd374e0f767a9f
Liz Gehret <elizabeth.gehret@gmail.com>
2022-12-09 15:14:50 -0700

    ....

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ import versioneer
 
 
 setup(
-    name='q2-composition',
+    name="q2-composition",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-    author='Jamie Morton',
-    author_email='jamietmorton@gmail.com',
-    description='Compositional statistics plugin for QIIME2.',
+    author="Jamie Morton",
+    author_email="jamietmorton@gmail.com",
+    description="Compositional statistics plugin for QIIME2.",
     scripts=['q2_composition/assets/run_ancombc.R'],
     license='BSD-3-Clause',
-    url='https://qiime2.org',
+    url="https://qiime2.org",
     entry_points={
         'qiime2.plugins':
         ['q2-composition=q2_composition.plugin_setup:plugin']
