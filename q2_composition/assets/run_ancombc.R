217f5e6ca94863d801115c89c7e5f0ceabd3cb58
Liz Gehret <elizabeth.gehret@gmail.com>
2024-05-02 15:53:29 -0700

    updating md import to use tidyverse

--- q2_composition/assets/run_ancombc.R
+++ q2_composition/assets/run_ancombc.R
@@ -70,10 +70,12 @@ if (!file.exists(inp_abundances_path)) {
   errQuit("Input file path does not exist.")
 } else {
   # Tidyverse
-  otu_file <- inp_abundances_path |>
     # The sample IDs are in the first column, which we treat as characters.
     # All other columns are feature frequencies as doubles
-    read_tsv(col_names = TRUE, col_types = cols("character", .default = col_double())) |>
+  otu_file <- read_tsv(inp_abundances_path,
+                       col_names = TRUE,
+                       col_types = cols("character",
+                                        .default = col_double())) |>
     # The first column is called "...1" during import, then we convert it to row names.
     column_to_rownames("...1") |>
     t()
@@ -82,10 +84,11 @@ if (!file.exists(inp_abundances_path)) {
 if (!file.exists(inp_metadata_path)) {
   errQuit("Metadata file path does not exist.")
 } else {
-  md_file <- read.delim(inp_metadata_path,
-                        check.names = FALSE, row.names = 1,
-                        colClasses = list(`sample-id` = "character"))
-  # We inforce meta.index being `sample-id` in _ancombc.py
+  md_file <- read_tsv(inp_metadata_path,
+                       col_names = TRUE,
+                       col_types = cols(`sample-id` = col_character())) |>
+    # We enforce meta.index being `sample-id` in _ancombc.py
+    column_to_rownames("sample-id")
   }
 
 # convert column types to numeric/categorical as specified in metadata
