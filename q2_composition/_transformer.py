a8f943e35ec0f56dc4f9a445418a27cb9a07ea5e
Evan Bolyen <evan@cym.is>
2025-04-22 16:19:58 -0700

    Support tabulate

--- q2_composition/_transformer.py
+++ q2_composition/_transformer.py
@@ -6,6 +6,8 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import json
+
 import pandas as pd
 
 from qiime2.sdk import ValidationError
@@ -15,6 +17,8 @@ from q2_composition._format import (
     ANCOMBC2OutputDirFmt,
     ANCOMBC2SliceMapping,
     FrictionlessCSVFileFormat,
+    DataLoafPackageDirFmt,
+    DataPackageSchemaFileFormat
 )
 
 
@@ -24,6 +28,25 @@ def _1(obj: FrictionlessCSVFileFormat) -> pd.DataFrame:
     df = pd.read_csv(str(path))
 
     return df
+
+
+@plugin.register_transformer
+def _12(format: DataLoafPackageDirFmt) -> ANCOMBC2SliceMapping:
+
+    slice_md = format.nutrition_facts.view(DataPackageSchemaFileFormat)
+    with open(str(slice_md)) as fh:
+        intercepts = json.load(fh)['metadata']['intercept_groups']
+        if type(intercepts) is str:
+            intercepts = [intercepts]
+
+    slices = ANCOMBC2SliceMapping()
+    slices.intercepts = intercepts
+
+    for (slice_path, slice_df) in format.data_slices.iter_views(pd.DataFrame):
+        slice_name = slice_path.name.split('_slice')[0]
+        slices[slice_name] = slice_df
+
+    return slices
 
 
 @plugin.register_transformer
@@ -58,5 +81,16 @@ def _3(format: ANCOMBC2OutputDirFmt) -> ANCOMBC2SliceMapping:
         except ValidationError:
             # optional structurual_zeros bound file not present on disk
             pass
+
+    intercepts = {}  # dict to preserve order instead of set()
+    _first_slice = slices[format.REQUIRED_SLICES[0]]
+    for column in _first_slice.columns:
+        extra = _first_slice[column].attrs.get('extra', {})
+        reference = extra.get('reference')
+        variable = extra.get('variable')
+        if reference and variable:
+            intercepts[f'{variable}::{reference}'] = True
+
+    slices.intercepts = list(intercepts)
 
     return slices
