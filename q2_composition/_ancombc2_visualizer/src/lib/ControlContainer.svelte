9cd58b6e2b035f42850c0ba1e981d5d6b0f41003
Macabe Wood <macabewood@gmail.com>
2026-05-19 14:27:43 -0700

    renaming folder

--- q2_composition/_ancombc2_visualizer/src/lib/ControlContainer.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    let { children, title } = $props();
-</script>
-
-<div class="w-full rounded bg-gray-50">
-    <h3 class="font-bold bg-purple-100 rounded-t px-4 py-1">{title}</h3>
-    <div class="px-4 py-2 max-h-64 overflow-auto">{@render children()}</div>
-</div>
--- q2_composition/_ancombc2_visualizer/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [svelte(), tailwindcss()],
-    base: "",
-});
