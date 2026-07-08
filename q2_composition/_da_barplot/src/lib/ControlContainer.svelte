dfd140abbb597b13c4b6d55c908bd530f71a1997
Colin Wood <woodcolin38@gmail.com>
2026-07-08 16:50:33 -0700

    layout

--- q2_composition/_da_barplot/src/lib/ControlContainer.svelte
+++ q2_composition/_da_barplot/src/lib/ControlContainer.svelte
@@ -4,5 +4,5 @@
 
 <div class="w-full rounded bg-gray-50">
     <h3 class="font-bold bg-purple-100 rounded-t px-4 py-1">{title}</h3>
-    <div class="px-4 py-2 max-h-64 overflow-auto">{@render children()}</div>
+    <div class="px-4 py-2 max-h-64 overflow-scroll">{@render children()}</div>
 </div>
