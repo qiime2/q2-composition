45c02264c30851dc5b693c681009c68a2ce86ba3
Macabe Wood <macabewood@gmail.com>
2026-07-14 16:55:47 -0700

    IMP: Transformer from `ANCOMBC2OutputDirFmt` to `Metadata` (#170)

--- q2_composition/_transformer.py
+++ q2_composition/_transformer.py
@@ -11,7 +11,9 @@ import os
 
 import pandas as pd
 
+from qiime2 import Metadata
 from qiime2.sdk import ValidationError
+from rachis.plugin.util import transform
 
 from q2_composition.plugin_setup import plugin
 from q2_composition._format import (
@@ -61,7 +63,7 @@ def _12(format: DataLoafPackageDirFmt) -> ANCOMBC2SliceMapping:
 @plugin.register_transformer
 def _2(slices: ANCOMBC2SliceMapping) -> ANCOMBC2OutputDirFmt:
     '''
-    Transforms a dataframe of ANCOMBC2 model statistics into the ANCOMBC2
+    Transforms a dictionary of ANCOMBC2 model statistics into the ANCOMBC2
     output directory format.
     '''
     format = ANCOMBC2OutputDirFmt()
@@ -132,6 +134,30 @@ def _3(format: ANCOMBC2OutputDirFmt) -> ANCOMBC2SliceMapping:
 
 @plugin.register_transformer
 def _5(format: DataLoafPackageDirFmt) -> ANCOMBC2OutputDirFmt:
-    slice_format = _12(format)
-    ancombc2_format = _2(slice_format)
+    slice_format = transform(format, to_type=ANCOMBC2SliceMapping)
+    ancombc2_format = transform(slice_format, to_type=ANCOMBC2OutputDirFmt)
     return ancombc2_format
+
+
+@plugin.register_transformer
+def _6(format: ANCOMBC2OutputDirFmt) -> Metadata:
+    slices = transform(format, to_type=ANCOMBC2SliceMapping)
+
+    merged = None
+    for slice, df in slices.items():
+        df = df.set_index(df['taxon'])
+        df = df.drop(columns='taxon')
+        df.index.name = 'feature-id'
+        df.columns = str(slice) + '_' + df.columns
+        if merged is None:
+            merged = df
+            continue
+        merged = pd.merge(
+            merged, df, left_index=True,  right_index=True, how='outer'
+        )
+
+    merged = merged.replace({True: 'True', False: 'False'})
+
+    md = Metadata(merged)
+
+    return md
